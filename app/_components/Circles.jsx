346f3e2ed40108b142e751ad8b4e3599a83c2df9
muhammedraouf1992 <muhammedraouf92@gmail.com>
2024-11-05 16:00:23 +0200

    add animation to circles section and featured work

--- app/_components/Circles.jsx
+++ app/_components/Circles.jsx
@@ -1,10 +1,45 @@
+"use client";
+
 import React from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useRef } from "react";
+import { useGSAP } from "@gsap/react";
 
+gsap.registerPlugin(ScrollTrigger);
 const Circles = () => {
+  const astraRef = useRef(null);
+
+  useGSAP(() => {
+    gsap.fromTo(
+      gsap.utils.toArray(".astraRef"),
+      {
+        y: 40,
+        opacity: 0,
+        duration: 1,
+        ease: "power3.inOut",
+      },
+      {
+        y: 0,
+        opacity: 1,
+        duration: 1,
+        ease: "power3.inOut",
+        stagger: 0.3,
+        scrollTrigger: {
+          trigger: astraRef.current,
+          start: "top 70%",
+          end: "top 30%",
+        },
+      }
+    );
+  }, []);
   return (
-    <section className="flex flex-col lg:flex-row justify-center gap-4 lg:gap-0">
-      <div className="border rounded-lg lg:rounded-full lg:-ml-20 border-white/20">
-        <div className="py-10 lg:py-0 lg:w-[400px] lg:h-[400px] flex flex-col items-center justify-center ">
+    <section
+      className="flex flex-col lg:flex-row justify-center gap-4 lg:gap-0"
+      ref={astraRef}
+    >
+      <div className="border rounded-lg lg:rounded-full lg:-ml-20 border-white/20 ">
+        <div className="py-10 lg:py-0 lg:w-[400px] lg:h-[400px] flex flex-col items-center justify-center astraRef">
           <p className="mb-3 lg:mb-0 text-5xl lg:text-8xl">25K</p>
           <p className="text-lg lg:text-xl text-center">
             <span className="font-bold"> Project</span> <br /> completed
@@ -12,8 +47,8 @@ const Circles = () => {
         </div>
       </div>
 
-      <div className="border rounded-lg lg:rounded-full lg:-ml-20 border-white/20">
-        <div className="py-10 lg:py-0 lg:w-[400px] lg:h-[400px] flex flex-col items-center justify-center ">
+      <div className="border rounded-lg lg:rounded-full lg:-ml-20 border-white/20 ">
+        <div className="py-10 lg:py-0 lg:w-[400px] lg:h-[400px] flex flex-col items-center justify-center astraRef">
           <p className="mb-3 lg:mb-0 text-5xl lg:text-8xl">25K</p>
           <p className="text-lg lg:text-xl text-center">
             <span className="font-bold"> Project</span> <br /> completed
@@ -21,8 +56,8 @@ const Circles = () => {
         </div>
       </div>
 
-      <div className="border rounded-lg lg:rounded-full lg:-ml-20 border-white/20">
-        <div className="py-10 lg:py-0 lg:w-[400px] lg:h-[400px] flex flex-col items-center justify-center ">
+      <div className="border rounded-lg lg:rounded-full lg:-ml-20 border-white/20 ">
+        <div className="py-10 lg:py-0 lg:w-[400px] lg:h-[400px] flex flex-col items-center justify-center astraRef">
           <p className="mb-3 lg:mb-0 text-5xl lg:text-8xl">25K</p>
           <p className="text-lg lg:text-xl text-center">
             <span className="font-bold"> Project</span> <br /> completed
@@ -30,8 +65,8 @@ const Circles = () => {
         </div>
       </div>
 
-      <div className="border rounded-lg lg:rounded-full lg:-ml-20 border-white/20">
-        <div className="py-10 lg:py-0 lg:w-[400px] lg:h-[400px] flex flex-col items-center justify-center ">
+      <div className="border rounded-lg lg:rounded-full lg:-ml-20 border-white/20 ">
+        <div className="py-10 lg:py-0 lg:w-[400px] lg:h-[400px] flex flex-col items-center justify-center astraRef">
           <p className="mb-3 lg:mb-0 text-5xl lg:text-8xl">25K</p>
           <p className="text-lg lg:text-xl text-center">
             <span className="font-bold"> Project</span> <br /> completed
--- app/_components/FeaturedWork.jsx
+++ app/_components/FeaturedWork.jsx
@@ -1,11 +1,70 @@
+"use client";
 import Image from "next/image";
-import React from "react";
+import React, { useRef } from "react";
 import work1 from "@/public/Shot.jpg";
 import work2 from "@/public/Shot-1.jpg";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
 const FeaturedWork = () => {
+  const astraRef = useRef(null);
+  const astraRef1 = useRef(null);
+  const astraRef2 = useRef(null);
+
+  useGSAP(() => {
+    gsap.fromTo(
+      astraRef1.current,
+      {
+        x: -10,
+        opacity: 0,
+        duration: 1,
+        ease: "power3.out",
+      },
+      {
+        x: 0,
+        opacity: 1,
+        duration: 1,
+        ease: "power3.inOut",
+        scrollTrigger: {
+          trigger: astraRef1.current,
+          start: "top 70%",
+          end: "top 30%",
+        },
+      }
+    );
+
+    gsap.fromTo(
+      astraRef2.current,
+      {
+        x: 10,
+        opacity: 0,
+        duration: 1,
+        ease: "power3.out",
+      },
+      {
+        x: 0,
+        opacity: 1,
+        duration: 1,
+
+        ease: "power3.inOut",
+        scrollTrigger: {
+          trigger: astraRef2.current,
+          start: "top 70%",
+          end: "top 30%",
+          markers: true,
+        },
+      }
+    );
+  }, []);
   return (
-    <section className="flex flex-col my-20 lg:flex-row lg:justify-between lg:px-28 gap-5 lg:gap-0">
-      <div>
+    <section
+      className="flex flex-col my-20 lg:flex-row lg:justify-between lg:px-28 gap-5 lg:gap-0"
+      ref={astraRef}
+    >
+      <div className="astraref" ref={astraRef1}>
         <div>
           <Image
             src={work1}
@@ -23,7 +82,7 @@ const FeaturedWork = () => {
         </div>
       </div>
 
-      <div>
+      <div className="astraref" ref={astraRef2}>
         <div>
           <Image
             src={work2}
